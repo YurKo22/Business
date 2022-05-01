55fee457ea4ba70be481ebb774278c95de745467
Yurii Kosach <81816470+YurKo22@users.noreply.github.com>
2022-05-01 19:05:54 +0300

    1.0.5
    gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+
+/* ========= Server ========== */
+
+gulp.task("server", function () {
+  browserSync.init({
+    server: {
+      port: 9000,
+      baseDir: "build",
+    },
+  });
+
+   gulp.watch("build/**/*").on('change', browserSync.reload);
+});
