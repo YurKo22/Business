d1c42e06edd6aaed270636063227072c082ee4ba
Yurii Kosach <81816470+YurKo22@users.noreply.github.com>
2022-05-01 21:36:23 +0300

    1.0.7

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,33 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync").create();
-const pug = require("gulp-pug");
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
 const rimraf = require('rimraf');
 const rename = require('gulp-rename');
 
 
-/* ========= Server ========== */
 
-gulp.task("server", function () {
+
+/* -------- Server  -------- */
+gulp.task('server', function() {
   browserSync.init({
     server: {
       port: 9000,
-      baseDir: "build",
-    },
+      baseDir: "build"
+    }
   });
 
-   gulp.watch("build/**/*").on('change', browserSync.reload);
+  gulp.watch('build/**/*').on('change', browserSync.reload);
 });
 
-/* ========= Pug compile ========== */
-gulp.task("templates:compile", function buildHTML() {
-  return gulp
-    .src("source/template/index.pug")
-    .pipe(
-      pug({
-        pretty: true,
-      })
-    )
-    .pipe(gulp.dest("build"));
+/* ------------ Pug compile ------------- */
+gulp.task('templates:compile', function buildHTML() {
+  return gulp.src('source/template/index.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('build'))
 });
 
 /* ------------ Styles compile ------------- */
